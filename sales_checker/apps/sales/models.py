7d660aac9b8078487e24b94918ebc6f7c534dfbf
Yuriy Dyachuk <dyachuk.yu@gmail.com>
2021-05-09 13:15:38 +0300

    Added sales model

---
+++ sales_checker/apps/sales/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+# /html/body/div[2]/main/div[2]/div/div[1]/div[3]/form/div/div/div/div/div[1]/div[1]/div[1]/div[1]/span
+class Sale(models.Model):
+    name = models.CharField(max_length=255, null=True, blank=True)
+    url = models.URLField(max_length=255, null=False, blank=False)
+    xpath = models.CharField(max_length=255, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'sales'
--- sales_checker/settings.py
+++ sales_checker/settings.py
@@ -27,6 +27,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # apps
+    'sales_checker.apps.sales',
 ]
 
 MIDDLEWARE = [
